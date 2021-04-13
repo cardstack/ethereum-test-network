75868bdc51eff68971e50d44c64b3c25f14d1e1d
Robertosnap <robertosnap@pm.me>
2021-04-13 13:00:20 +0200

    added erc20 test

--- src/TestNetwork.test.ts
+++ src/TestNetwork.test.ts
@@ -1,8 +1,9 @@
 import { ethers } from 'ethers';
 import { TestNetwork } from './TestNetwork';
+import { ERC20Token__factory } from './utils/ERC20Token__factory';
 
 const JSON_RPC_PROVIDER = 'http://localhost:8545';
-describe('testnt function', () => {
+describe('eth test network', () => {
   let testnetwork: TestNetwork;
 
   // Act before assertions
@@ -42,5 +43,32 @@ describe('testnt function', () => {
     await tx.wait();
     const balance = await provider.getBalance(randomWallet.address);
     expect(value.eq(balance)).toBeTruthy();
+  });
+
+  it('should deploy erc20 and send token', async () => {
+    const provider = new ethers.providers.JsonRpcProvider(JSON_RPC_PROVIDER);
+    const wallet = new ethers.Wallet(
+      '0xa3dac6ca0b1c61f5f0a0b3a0acf93c9a52fd94e8e33d243d3b3a8b8c5dc37f0b',
+    ).connect(provider);
+    const erc20 = await new ERC20Token__factory(wallet).deploy(
+      'Test token',
+      'tst',
+      18,
+    );
+    await erc20.deployed();
+    const randomWallet = ethers.Wallet.createRandom();
+    const mintTx = await erc20.mint(
+      wallet.address,
+      ethers.utils.parseEther('500'),
+    );
+    await mintTx.wait();
+    const transferValue = ethers.utils.parseEther('10');
+    const transferTx = await erc20.transfer(
+      randomWallet.address,
+      transferValue,
+    );
+    await transferTx.wait();
+    const coinBalance = await erc20.balanceOf(randomWallet.address);
+    expect(coinBalance.gte(transferValue)).toBeTruthy();
   });
 });
